2c3adfe953f790d145eb8c4d41a90b65845e079f
AkashDavid416 <hiakashdavid@gmail.com>
2025-06-18 15:01:44 +0530

    Add files via upload

---
+++ Positive or Negative.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter a Number:";
+    cin>>a;
+    if(a>0)
+    {
+        cout<<"It is a positive Number";
+    }
+    else
+    {
+        cout<<"It is a negative Number";
+    }
+}
