1eb628a069c8c6dfba51d69f07a74eacba6c00cf
AkashDavid416 <hiakashdavid@gmail.com>
2025-06-18 15:03:10 +0530

    Add files via upload

---
+++ Greatest of 2 Numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter a Number One:";
+    cin>>a;
+    cout<<"Enter a Number Two:";
+    cin>>b;
+    if(a>b)
+    {
+        cout<<a<<" is the greatest number";
+    }
+    else
+    {
+        cout<<b<<" is the greatest number";
+    }
+}
